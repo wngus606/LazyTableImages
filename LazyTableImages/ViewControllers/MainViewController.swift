bbfc28e6d0de1559613a5e369ece174e3121da05
seoju <wngus606@gmail.com>
2018-06-20 11:27:51 +0900

    first commit

---
+++ LazyTableImages/ViewControllers/MainViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MainViewController.swift
+//  LazyTableImages
+//
+//  Created by seoju on 2018. 6. 20..
+//  Copyright © 2018년 seoju. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIAlertController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
