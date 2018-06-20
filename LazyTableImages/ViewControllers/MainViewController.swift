c19c3f50acc5c8072beec12b2135ec9f7e486af9
seoju <wngus606@gmail.com>
2018-06-20 11:38:39 +0900

    modify Filename

--- LazyTableImages/ViewControllers/MainViewController.swift
+++ LazyTableImages/ViewControllers/MainViewController.swift
@@ -8,9 +8,11 @@
 
 import UIKit
 
-class MainViewController: UIAlertController {
+class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
     }
 }
